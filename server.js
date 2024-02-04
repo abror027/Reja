375131c2f4e8a7d2d52342762a2fcf96aab4c9af
MAKHKAMOV ABRORJON <157876789+abror027@users.noreply.github.com>
2024-02-04 23:51:22 +0900

    BRR: EJS 18-19

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-console.log("web serverni boshlash");
+/* console.log("web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
 
-// 1 - kirish codelari (expressdankirib kelayotgan ma'lumotlarga bog'liq kodlar)
+// 1 - kirish codelari (expressdan kirib kelayotgan ma'lumotlarga bog'liq kodlar)
 app.use(express.static("publik"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -31,5 +31,45 @@ const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
     console.log(`The server is running successfully on port: ${PORT}`);
+}); */
+
+// EJS framework orqali ananaviy fronted quramiz
+
+console.log("web serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1 - kirish codelari (expressdan kirib kelayotgan ma'lumotlarga bog'liq kodlar)
+app.use(express.static("publik"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2 - sessionga bog'liq narsalar
+
+// 3 -Views codes (bssr => backend server site randrung(backenda html yasab clenga yuborish))
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 - Routinglarga mo'ljallangan
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success"});
 });
+
+app.get("/", function (req, res) {
+    res.render("harid");
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
+
+
+
+
+
 
