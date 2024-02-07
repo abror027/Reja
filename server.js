4afbf326d00de99230ebe6609dc79f17a30f9c9c
MAKHKAMOV ABRORJON <157876789+abror027@users.noreply.github.com>
2024-02-07 19:34:50 +0900

    feat: develop author router esj page

--- server.js
+++ server.js
@@ -1,37 +1,3 @@
-/* console.log("web serverni boshlash");
-const express = require("express");
-const app = express();
-const http = require("http");
-
-// 1 - kirish codelari (expressdan kirib kelayotgan ma'lumotlarga bog'liq kodlar)
-app.use(express.static("publik"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// 2 - sessionga bog'liq narsalar
-
-// 3 -Views codes (bssr => backend server site randrung(backenda html yasab clenga yuborish))
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 - Routinglarga mo'ljallangan
-
-// app.get("/", function(req, res) {
-//     res.end(`<h1 style="background: red">Hello! Hush kelibsiz<h/1>`)
-// });
-
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="background: red">Hello! Hush kelibsiz by GaryAli<h/1>`)
-}); 
-app.get("/gift", function(req, res) {
-    res.end(`<h1 style="background: green">Siz sovg'alar bolimidasiz<h/1>`)
-}); 
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT}`);
-}); */
 
 // EJS framework orqali ananaviy fronted quramiz
 
@@ -39,9 +5,19 @@ console.log("web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    }else {
+        user =JSON.parse(data);
+    }
+});
 
 // 1 - kirish codelari (expressdan kirib kelayotgan ma'lumotlarga bog'liq kodlar)
-app.use(express.static("publik"));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -57,8 +33,12 @@ app.post("/create-item", (req, res) => {
     res.json({ test: "success"});
 });
 
+app.get('./author', (req, res) => {
+    res.render("author", {user: user});
+});
+
 app.get("/", function (req, res) {
-    res.render("harid");
+    res.render("");
 });
 
 
