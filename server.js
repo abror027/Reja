1fc4398e5fe054581dd9aacea2dccfff2da7f936
MAKHKAMOV ABRORJON <157876789+abror027@users.noreply.github.com>
2024-02-08 17:15:12 +0900

    feat: Finish Portifolio first page

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post("/create-item", (req, res) => {
     res.json({ test: "success"});
 });
 
-app.get('./author', (req, res) => {
+app.get('/author', (req, res) => {
     res.render("author", {user: user});
 });
 
